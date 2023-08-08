b0c1749234143fa46964ddf05c2c5f81bc4e8a39
iramaroliveira <iramar.oliveira@trc.sebraeba.com.br>
2023-08-08 10:37:26 -0300

    Feito ajustes na tela de cadastrar cliente

--- src/pages/forgot-password/forgot-password.vue
+++ src/pages/forgot-password/forgot-password.vue
@@ -7,8 +7,10 @@
         <div class="form-init">
             <a-col :span="14">
                 <a-form layout="vertical" name="basic" :model="data" @finish="forgotPassword" :hideRequiredMark="true">
-                    <h3>Esqueceu sua senha?</h3>
-                    <h5>Para recuperar seu acesso, precisamos do seu E-mail.</h5>
+                    <div class="title">
+                        <h2>Esqueceu sua senha?</h2>
+                        <h5>Para recuperar seu acesso, precisamos do seu e-mail.</h5>
+                    </div>
                     <a-form-item label="E-mail" name="email"
                         :rules="[{ required: true, message: 'Campo e-mail é obrigatório' }]">
                         <a-input v-model:value="data.email" size="large" />
@@ -31,7 +33,6 @@
 </template>
 
 <script>
-import './forgot-password.css';
 import axios from '../../services/api.js';
 
 export default {
--- src/pages/register/client/client.vue
+++ src/pages/register/client/client.vue
@@ -7,94 +7,103 @@
         <div class="form-init">
             <a-col :span="20" class="register-client">
                 <a-form layout="vertical" name="basic" :model="data" @finish="save" :hideRequiredMark="true">
-                    <h3>Cadastre-se</h3>
-                    <h5>Cadastre-se e tenha acesso as melhores ofertas de veículos do Brasil.</h5>
-
-                    <a-form-item label="Nome" name="name"
-                        :rules="[{ required: true, message: 'Campo nome é obrigatório' }]">
-                        <a-input v-model:value="data.name" size="large" />
-                    </a-form-item>
+                    <div class="title">
+                        <h2>Cadastre-se</h2>
+                        <h5>Cadastre-se e tenha acesso as melhores ofertas de veículos do Brasil.</h5>
+                    </div>
 
                     <a-row :gutter="[8, 16]">
-                        <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" :md="{ span: 24 }" :xl="{ span: 12 }">
-                            <a-form-item label=" E-mail" name="email"
-                                :rules="[{ required: true, message: 'Campo e-mail é obrigatório' }]">
-                                <a-input v-model:value="data.email" size="large" />
+                        <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" :xl="{ span: 12 }">
+                            <a-form-item label="Nome" name="name"
+                                :rules="[{ required: true, message: 'Campo nome é obrigatório' }]">
+                                <a-input v-model:value="data.name" />
                             </a-form-item>
                         </a-col>
-                        <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" :md="{ span: 24 }" :xl="{ span: 12 }">
-                            <a-form-item label="Senha" name="password"
-                                :rules="[{ required: true, message: 'Campo senha é obrigatório' }]">
-                                <a-input-password v-model:value="data.password" size="large" />
-                            </a-form-item>
-                        </a-col>
-                    </a-row>
-
-                    <a-row :gutter="[8, 16]">
-                        <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" :md="{ span: 24 }" :xl="{ span: 12 }">
+                        <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" :xl="{ span: 12 }">
                             <a-form-item label="CPF" name="cpf"
                                 :rules="[{ required: true, message: 'Campo cpf é obrigatório' }]">
-                                <a-input v-model:value="data.cpf" size="large" />
+                                <a-input v-model:value="data.cpf" />
                             </a-form-item>
                         </a-col>
-                        <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" :md="{ span: 24 }" :xl="{ span: 12 }">
+                    </a-row>
+
+                    <a-row :gutter="[8, 16]">
+                        <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" :xl="{ span: 12 }">
+                            <a-form-item label=" E-mail" name="email"
+                                :rules="[{ required: true, message: 'Campo e-mail é obrigatório' }]">
+                                <a-input v-model:value="data.email" />
+                            </a-form-item>
+                        </a-col>
+                        <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" :xl="{ span: 12 }">
+                            <a-form-item label="Senha" name="password"
+                                :rules="[{ required: true, message: 'Campo senha é obrigatório' }]">
+                                <a-input-password v-model:value="data.password" />
+                            </a-form-item>
+                        </a-col>
+                    </a-row>
+
+                    <a-row :gutter="[8, 16]">
+                        <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" :xl="{ span: 12 }">
+                            <a-form-item label="Telefone" name="phone"
+                                :rules="[{ required: true, message: 'Campo cpf é obrigatório' }]">
+                                <a-input v-model:value="data.cpf" />
+                            </a-form-item>
+                        </a-col>
+                        <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" :xl="{ span: 12 }">
                             <a-form-item label="CEP" name="cep"
                                 :rules="[{ required: true, message: 'Campo cep é obrigatório' }]">
-                                <a-input v-model:value="data.cep" size="large" />
+                                <a-input v-model:value="data.cep" />
                             </a-form-item>
                         </a-col>
                     </a-row>
 
                     <a-row :gutter="[8, 16]">
-                        <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" :md="{ span: 24 }" :xl="{ span: 12 }">
+                        <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" :xl="{ span: 12 }">
                             <a-form-item label="Endereço" name="address">
-                                <a-input v-model:value="data.address" size="large" disabled />
+                                <a-input v-model:value="data.address" disabled />
                             </a-form-item>
                         </a-col>
-                        <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" :md="{ span: 24 }" :xl="{ span: 12 }">
+                        <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" :xl="{ span: 12 }">
                             <a-form-item label="Complemento" name="complement">
-                                <a-input v-model:value="data.complement" size="large" />
+                                <a-input v-model:value="data.complement" />
                             </a-form-item>
                         </a-col>
                     </a-row>
 
                     <a-row :gutter="[8, 16]">
-                        <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" :md="{ span: 24 }" :xl="{ span: 12 }">
+                        <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" :xl="{ span: 12 }">
                             <a-form-item label="UF" name="uf">
-                                <a-input v-model:value="data.uf" size="large" disabled />
+                                <a-input v-model:value="data.uf" disabled />
                             </a-form-item>
                         </a-col>
-                        <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" :md="{ span: 24 }" :xl="{ span: 12 }">
+                        <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" :xl="{ span: 12 }">
                             <a-form-item label="cidade" name="city">
-                                <a-input v-model:value="data.city" size="large" disabled />
+                                <a-input v-model:value="data.city" disabled />
                             </a-form-item>
                         </a-col>
                     </a-row>
 
                     <a-row :gutter="[8, 16]">
-                        <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" :md="{ span: 24 }" :xl="{ span: 12 }">
+                        <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" :xl="{ span: 12 }">
                             <a-form-item label="Bairro" name="neighborhood">
-                                <a-input v-model:value="data.neighborhood" size="large" disabled />
+                                <a-input v-model:value="data.neighborhood" disabled />
                             </a-form-item>
                         </a-col>
-                        <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" :md="{ span: 24 }" :xl="{ span: 12 }">
+                        <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" :xl="{ span: 12 }">
                             <a-form-item label="Número" name="number"
                                 :rules="[{ required: true, message: 'Campo número é obrigatório' }]">
-                                <a-input v-model:value="data.number" size="large" />
+                                <a-input v-model:value="data.number" />
                             </a-form-item>
                         </a-col>
                     </a-row>
 
-                    <a-form-item>
-                        <a-button type="primary" html-type="submit" class="button-init" size="large">Entrar</a-button>
-                    </a-form-item>
-
-                    <!-- <div class="link-register">
-                    <span>nÃ£o tem cadastro?</span>
-                    <router-link to="/cadastrar-cliente">
-                        <strong>clique aqui</strong>
+                    <router-link to="/login" class="link-forgot">
+                        voltar e fazer Login
                     </router-link>
-                </div> -->
+
+                    <a-form-item>
+                        <a-button type="primary" html-type="submit" class="button-init">Cadastrar</a-button>
+                    </a-form-item>
                 </a-form>
             </a-col>
         </div>
@@ -114,6 +123,7 @@ export default {
                 email: null,
                 password: null,
                 cpf: null,
+                phone: null,
                 cep: null,
                 address: null,
                 complement: null,
