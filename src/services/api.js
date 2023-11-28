f5700898d81e738629b56b524edc4c633e87fadd
iramaroliveira <iramar.oliveira@trc.sebraeba.com.br>
2023-11-28 16:03:22 -0300

    Alterado endpoint backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://ec2-18-212-187-130.compute-1.amazonaws.com:8080',
+    baseURL: 'https://api.dev.alugueaqui.xyz:8080',
+    // baseURL: 'http://ec2-18-212-187-130.compute-1.amazonaws.com:8080',
     // baseURL: 'http://localhost:8080',
 });
 
